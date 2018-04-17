52b8c5cb4db0f6a06635d01ca95cd9aec39cf93b
Matt Bloomfield <mbloomfield@uschamber.com>
2018-04-17 08:24:04 -0400

    Adding ability to serve static files

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(express.static(path.join(__dirname, '../leaders-and-laggards-angularjs/public')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../leaders-and-laggards-angularjs/public/index.html')))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
